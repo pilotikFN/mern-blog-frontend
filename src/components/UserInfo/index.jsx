9036f6a0ca5158dc4399273e376602aea2b2d209
jorik <spiridonovjar@gmail.com>
2024-01-13 12:55:25 +0400

    First editing

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './UserInfo.module.scss';
 
-export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
+export const UserInfo = ({ avatarUrl, name, additionalText }) => {
   return (
     <div className={styles.root}>
-      <img className={styles.avatar} src={avatarUrl || '/noavatar.png'} alt={fullName} />
+      <img className={styles.avatar} src={avatarUrl || '/noavatar.png'} alt={name.first} />
       <div className={styles.userDetails}>
-        <span className={styles.userName}>{fullName}</span>
+        <span className={styles.userName}>{name.first} {name.last}</span>
         <span className={styles.additional}>{additionalText}</span>
       </div>
     </div>
